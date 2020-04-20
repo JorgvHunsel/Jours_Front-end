a23543935dd20153e588688a953a7f3225a48f7d
Jorg Van Hunsel <jorg.van.hunsel@gmail.com>
2020-04-20 15:47:36 +0200

    het is nu mogelijk om een work toe tevoegen

--- src/pages/AddWorkPage.js
+++ src/pages/AddWorkPage.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import auth from '../service/auth'
 import jwt from 'jsonwebtoken'
 
+import DatePicker from 'react-datepicker'
+
+
 import { Row, Container, Col, Button, Dropdown, DropdownButton } from 'react-bootstrap';
 
 class AddWorkPage extends Component {
@@ -11,21 +14,30 @@ class AddWorkPage extends Component {
         this.state = {
             companies: [],
             projects: [],
-            beginTime: '',
-            endTime: '',
+            projectId: '',
+            beginDate: new Date(),
+            endDate: new Date(),
         }
+
     }
 
-    handleCompanyNameChange = event => {
+    handleBeginDate(time) {
         this.setState({
-            companyName: event.target.value
+            beginDate: time
+        })
+    }
+
+    handleEndDate(time) {
+        this.setState({
+            endDate: time
         })
     }
 
     handleSubmit = (e) => {
+        console.log(this.state.beginDate)
         console.log(auth)
         e.preventDefault();
-        fetch('http://localhost:8090/company/create', {
+        fetch('http://localhost:8090/work/add', {
             method: 'POST',
             headers: {
                 'Accept': 'application/json',
@@ -33,8 +45,9 @@ class AddWorkPage extends Component {
                 'Authorization': 'Bearer ' + window.sessionStorage.getItem("userToken"),
             },
             body: JSON.stringify({
-                companyName: this.state.companyName,
-                userId: window.sessionStorage.getItem("userId")
+                projectId: 24,
+                beginDate: this.state.beginDate,
+                endDate: this.state.endDate                
             })
         }).then(response => response.json())
             .then(data => {
@@ -48,14 +61,10 @@ class AddWorkPage extends Component {
             <React.Fragment>
                 <div>
                     <Container>
-                        <Row><Col><h3>Add work:</h3></Col></Row>
                         <Row>
                             <Col>
                                 <Dropdown>
-                                    <Dropdown.Toggle variant="primary" id="dropdown-basic">
-                                        Dropdown Button
-                                    </Dropdown.Toggle>
-
+                                    <Dropdown.Toggle variant="outline-info" id="dropdown-basic" block>Choose your company</Dropdown.Toggle>
                                     <Dropdown.Menu>
                                         <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
                                         <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
@@ -65,9 +74,7 @@ class AddWorkPage extends Component {
                             </Col>
                             <Col>
                                 <Dropdown>
-                                    <Dropdown.Toggle variant="primary" id="dropdown-basic">
-                                        Dropdown Button
-                                    </Dropdown.Toggle>
+                                    <Dropdown.Toggle variant="outline-info" id="dropdown-basic" block>Choose your project</Dropdown.Toggle>
 
                                     <Dropdown.Menu>
                                         <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
@@ -77,8 +84,8 @@ class AddWorkPage extends Component {
                                 </Dropdown>
                             </Col>
                         </Row>
+                        <Row><Col><h3>Add work:</h3></Col></Row>
                         <Row><Col><Button block>Clock in</Button></Col></Row>
-
                         <Row><Col><h3>Add work manually:</h3></Col></Row>
                         <form className="form">
                             <div className="div">
@@ -86,14 +93,29 @@ class AddWorkPage extends Component {
                                     <Col>
                                         <div className="form-group">
                                             <label>Begin time:</label>
-                                            <input required type="text" className="form-control" placeholder="Enter the name of your company" onChange={this.handleCompanyNameChange} />
+                                            <DatePicker
+                                                selected={this.state.beginDate}
+                                                showTimeSelect
+                                                showTimeSelectOnly
+                                                timeIntervals={15}
+                                                timeCaption="Time"
+                                                timeFormat="HH:mm"
+                                                dateFormat="HH:mm"
+                                                onChange={date => this.handleBeginDate(date)} />
                                         </div>
                                     </Col>
                                     <Col>
                                         <div className="form-group">
                                             <label>End time:</label>
-                                            <input required type="text" className="form-control" placeholder="Enter the name of your company" onChange={this.handleCompanyNameChange} />
-                                        </div>
+                                            <DatePicker
+                                                selected={this.state.endDate}
+                                                showTimeSelect
+                                                showTimeSelectOnly
+                                                timeIntervals={15}
+                                                timeCaption="Time"
+                                                timeFormat="HH:mm"
+                                                dateFormat="HH:mm"
+                                                onChange={date => this.handleEndDate(date)} />                                        </div>
                                     </Col>
                                 </Row>
 
